dc155b853b494d8e887dcb3f4424d224f2fb1c11
Burint172 <Burint172@gmail.com>
2026-07-27 12:53:16 +0700

    2513110581 บุรินทร์สุดเท่

--- Node.java
+++ Node.java
@@ -1,12 +1,17 @@
 public class Node {
-    public Object data;
-    public Node next;
+    // Attributes in Class Node
+    Node previous;
+    Object data;
+    Node next;
 
+    // Constructor
     public Node (Object data) {
+        this.previous = null;
         this.data = data;
         this.next = null;
     }
 
+    // Method for getting address in specific node
     public String getAddress() {
         return "" + Integer.toHexString(System.identityHashCode(this));
     }
@@ -14,7 +19,4 @@ public class Node {
     public static String getAddress(Node node) {
         return (node == null) ? "null" : node.getAddress();
     }
-
-
-
 }
